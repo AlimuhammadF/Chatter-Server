f6e001c94cbdc14b49ab4db7d9d19f899efea5ed
AlimuhammadF <Alimuhammadfp5@gmail.com>
2022-08-02 15:26:38 -0700

    starter temp

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-const dotenv = require("dotenv").config();
+require("dotenv").config();
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
+const cors = require("cors");
 
 // create http server
 const app = express();
 const server = http.createServer(app);
+
+// middleware
+app.use(cors());
+app.use(express.json());
 
 // create socket server
 const io = new Server(server, {
@@ -13,3 +18,12 @@ const io = new Server(server, {
 		origin: "*",
 	},
 });
+
+// routees
+app.get("/", (req, res) => {
+	res.send("Hello World");
+});
+
+// Listening
+const port = 8080 || process.env.PORT;
+server.listen(port, () => console.log(`Listening port:${port}`));
