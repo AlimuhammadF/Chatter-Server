437cb1baafa456195a0ca9b7c61558aa2749f19b
AlimuhammadF <Alimuhammadfp5@gmail.com>
2022-08-05 15:43:22 -0700

    create chat api

--- routes/chat/createChat.js
+++ routes/chat/createChat.js
@@ -1,9 +1,57 @@
 const express = require("express");
+const Chat = require("../../models/chatModel");
 const router = express.Router();
 
 // create chat api
-router.post("/", (req, res) => {
-	res.send("Create Chat");
+router.post("/", async (req, res) => {
+	// destructure bodu
+	const { receiverId, senderId } = req.body;
+
+	// check if personal chat exist with this user id
+	try {
+		const isChatExist = await Chat.findOne({
+			isGroupChat: false,
+			$and: [
+				{ members: { $elemMatch: { $eq: senderId } } },
+				{ members: { $elemMatch: { $eq: receiverId } } },
+			],
+		})
+			.populate("members", "-password")
+			.populate("latestMessage");
+
+		// if chat exit with this user id
+		if (isChatExist) {
+			res.status(200).json({
+				success: true,
+				message: "Chat already exist with the user.",
+				result: isChatExist,
+			});
+			res.end();
+			return;
+		}
+
+		// chat data
+		const chatData = {
+			members: [receiverId, senderId],
+		};
+
+		// if not exist create chat wuth this userid
+		const createChat = await (
+			await (await Chat.create(chatData)).populate("members", "-password")
+		).populate("latestMessage");
+
+		res.status(201).json({
+			success: true,
+			message: "Chat has been created.",
+			createChat,
+		});
+
+		res.end();
+	} catch (error) {
+		// if error
+		res.status(400).json({ error, message: "Creating chat unsuccessful" });
+		res.end();
+	}
 });
 
 // export
